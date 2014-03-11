9aa5563c50d97b9dd2419f5cd5869a5d47802d8a
Chuck Coughlin <chuck_coughlin@comcst.net>
2014-03-11 13:44:51 -0600

    -n AED- pca minor tweaks.

---
+++ workspace/BLT_Migration/src/com/ils/blt/migration/G2Block.java
@@ -0,0 +1,76 @@
+package com.ils.blt.migration;
+
+import java.util.UUID;
+
+/**
+ * Implement a plain-old-java-object representing a G2 process block.
+ * This is designed for serialization via a JSON serializer.
+ * 
+ * Use arrays instead of Java-generics lists to make this serializable.
+ * WARNING: Avoid use of Point class as it contains a circular reference
+ *          to itself.
+ */
+public class G2Block {
+	private int x = 0;
+	private int y = 0;
+	private UUID uuid = null;
+	private UUID originalId = null;       // Id of block from which this was cloned
+	private String className = null;
+	private String embeddedIcon="";       // 32x32 icon to place in block in designer
+	private String embeddedLabel="";      // Label place in block in designer
+	private int    embeddedFontSize = 24; // Pointsin font for embedded label
+	private String iconPath="";           // Path to icon that is the entire block
+	private int preferredHeight = 0;
+	private int preferredWidth  = 0;
+	private BlockStyle style = BlockStyle.BASIC;
+	private String label;
+	private String statusText;
+	private BlockState state;
+	private BlockProperty[] properties = null;
+	private SerializableAnchor[] anchors = null;
+	
+	public G2Block() {
+		this.anchors = new SerializableAnchor[0];
+	}
+	
+	public String getClassName() {return className;}
+	public void setClassName(String className) {this.className = className;}
+	public UUID getId() { return uuid; }
+	public void setId(UUID id) { uuid = id; }
+	public UUID getOriginalId() { return originalId; }
+	public void setOriginalId(UUID id) { originalId = id; }
+	public String getLabel() { return label; }
+	public void setLabel(String label) { this.label = label; }
+	public int getX() { return x; }
+	public int getY() { return y; }
+	public void setX(int xx) { this.x=xx; }
+	public void setY(int yy) { this.y=yy; }
+	public BlockStyle getStyle() { return style; }
+	public String getStatusText() { return statusText; }
+	public void setStatusText(String statusText) { this.statusText = statusText; }
+	public BlockState getState() { return state; }
+	public void setState(BlockState state) { this.state = state; }
+	public void setStyle(BlockStyle style) { this.style = style; }
+	public SerializableAnchor[] getAnchors() { return anchors; }
+	public void setAnchors(SerializableAnchor[] array) {
+		anchors = array;
+		for(SerializableAnchor sa:anchors) {
+			sa.setParentId(uuid);
+		}
+	}
+	public String getEmbeddedIcon() {return embeddedIcon;}
+	public void setEmbeddedIcon(String embeddedIcon) {this.embeddedIcon = embeddedIcon;}
+	public String getEmbeddedLabel() {return embeddedLabel;}
+	public void setEmbeddedLabel(String embeddedLabel) {this.embeddedLabel = embeddedLabel;}
+	public int getEmbeddedFontSize() {return embeddedFontSize;}
+	public void setEmbeddedFontSize(int embeddedFontSize) {this.embeddedFontSize = embeddedFontSize;}
+	public String getIconPath() {return iconPath;}
+	public void setIconPath(String iconPath) {this.iconPath = iconPath;}
+	public int getPreferredHeight() {return preferredHeight;}
+	public void setPreferredHeight(int preferredHeight) {this.preferredHeight = preferredHeight;}
+	public int getPreferredWidth() {return preferredWidth;}
+	public void setPreferredWidth(int preferredWidth) {this.preferredWidth = preferredWidth;}
+	public BlockProperty[] getProperties() { return properties; }
+	public void setProperties(BlockProperty[] array) { this.properties = array; }
+
+}
--- workspace/BLT_Migration/src/com/ils/blt/migration/Migrator.java
+++ workspace/BLT_Migration/src/com/ils/blt/migration/Migrator.java
@@ -3,6 +3,9 @@
  */
 package com.ils.blt.migration;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -15,11 +18,70 @@ import org.sqlite.JDBC;
 
 public class Migrator {
 	private static final String USAGE = "Usage: migrator <database>";
-	private final Map<String,String> classMap;   // Lookup by G2 classname
+	private final Map<String,String> classMap;     // Lookup by G2 classname
 	private final static JDBC driver = new JDBC(); // Force driver to be loaded
+	private boolean ok = true;                     // Allows us to short circuit processing
 	
-	public Migrator(Map<String,String>classLookup) {
-		this.classMap = classLookup;
+	public Migrator() {
+		classMap = new HashMap<String,String>();
+	}
+	
+	public void processDatabase(String path) {
+		String connectPath = "jdbc:sqlite:"+path;
+
+		// Read database to generate conversion maps
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection(connectPath);
+			Statement statement = connection.createStatement();
+			statement.setQueryTimeout(30);  // set timeout to 30 sec.
+			
+			ResultSet rs = statement.executeQuery("select * from ClassMap");
+			while(rs.next())
+			{
+				String g2 = rs.getString("G2Class");
+				String ignition = rs.getString("IgnitionClass");
+				classMap.put(g2, ignition);
+			}
+		}
+		catch(SQLException e) {
+			// if the error message is "out of memory", 
+			// it probably means no database file is found
+			System.err.println(e.getMessage());
+			ok = false;
+		}
+		finally {
+			try {
+				if(connection != null)
+					connection.close();
+			} 
+			catch(SQLException e) {
+				// connection close failed.
+				System.err.println(e);
+			}
+		}
+	}
+	
+	/**
+	 * Read standard input. Convert into G2 Diagram
+	 */
+	public void processInput() {
+		if( !ok ) return;
+		
+		// Read of stdin is expected to be from a re-directed file. 
+		// We gobble the whole thing here.
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer input = new StringBuffer();
+		String s = null;
+		try{
+			while ((s = in.readLine()) != null && s.length() != 0) {
+				input.append(s);
+			}
+		}
+		catch(IOException ignore) {}
+		
+		// Now convert into a G2 Diagram
+		
 	}
 	
 	
@@ -33,60 +95,23 @@ public class Migrator {
 	 * @param args command-line arguments
 	 */
 	public static void main(String[] args) {
-		
-		
+			
 		// Look for database path as an argument
 		if( args.length == 0) {
 			System.out.println(USAGE);
 			System.exit(1);
 		}
+		
+		Migrator m = new Migrator();
 		String path = args[0];
 		// In case we've been fed a Windows path, convert
 		path = path.replace("\\", "/");
-		String connectPath = "jdbc:sqlite:"+path;
-		//try {
-		//	Class.forName("org.sqlite.JDBC");    // Force loading of the driver class
-		//}
-		//catch(ClassNotFoundException cnfe) {}
-
-		Connection connection = null;
-		try
-		{
-			// create a database connection
-			connection = DriverManager.getConnection(connectPath);
-			Statement statement = connection.createStatement();
-			statement.setQueryTimeout(30);  // set timeout to 30 sec.
-
-			Map<String,String> classMap = new HashMap<String,String>();
-			ResultSet rs = statement.executeQuery("select * from ClassMap");
-			while(rs.next())
-			{
-				// read the result set
-				String g2 = rs.getString("G2Class");
-				String ignition = rs.getString("IgnitionClass");
-				classMap.put(g2, ignition);
-			}
-			Migrator m = new Migrator(classMap);
-		}
-		catch(SQLException e)
-		{
-			// if the error message is "out of memory", 
-			// it probably means no database file is found
-			System.err.println(e.getMessage());
-		}
-		finally
-		{
-			try
-			{
-				if(connection != null)
-					connection.close();
-			}
-			catch(SQLException e)
-			{
-				// connection close failed.
-				System.err.println(e);
-			}
-		}
+		m.processDatabase(path);
+		m.processInput();
+		m.migrate();
+		m.createOutput();
+		
+		
 	}
 
 }
