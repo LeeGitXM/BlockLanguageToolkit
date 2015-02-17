34fd21b2ab74ebfe43195e62826327373929ec8e
Chuck Coughlin <ccoughlin@ils-automation.com>
2015-02-17 12:11:06 -0700

    Merge block and procedure migration databases.

---
+++ migration/mdb/insertProcClassMap.sql
@@ -0,0 +1,2 @@
+insert into ProcClassMap values ('em-application','com.ils.blt.gateway.engine.ProcessApplication');
+insert into ProcClassMap values ('em-gda-final-diagnosis','ils.block.finaldiagnosis');
