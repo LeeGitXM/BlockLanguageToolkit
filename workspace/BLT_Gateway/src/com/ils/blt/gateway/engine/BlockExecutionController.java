7f073918117dde564fb82e9dbe22e1e57aa53d27
Chuck Coughlin <ccoughlin@ils-automation.com>
2018-01-19 15:06:18 -0700

    Fix startup error at Escorez.

--- workspace/BLT_Gateway/src/com/ils/blt/gateway/engine/BlockExecutionController.java
+++ workspace/BLT_Gateway/src/com/ils/blt/gateway/engine/BlockExecutionController.java
@@ -490,6 +490,10 @@ public class BlockExecutionController implements ExecutionController, Runnable {
 	}
 	@Override
 	public QualifiedValue getTagValue(UUID diagramId,String path) {
+		if( tagReader==null || path==null) {
+			log.warnf("%s.getTagValue: tagReader %s and tagPath %s",TAG,(tagReader==null?"NULL":"NOT NULL"),(path==null?"NULL":"NOT NULL"));
+			return null;
+		}
 		return tagReader.readTag(path);
 	}
 	@Override
